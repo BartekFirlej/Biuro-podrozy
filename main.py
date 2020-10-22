3dc9a4bdf018be2589ece11b3885aba38d8d1658
BartekFirlej <yari12@wp.pl>
2020-10-22 16:48:22 +0200

    Wyszukuje i wypisuje wycieczki

--- main.py
+++ main.py
@@ -22,42 +22,45 @@ def take_data():
         type = input("T-turystyczna I-imprezowa C-chillowa. Wybierz swój typ: ")            #Daje kolejna szanse
     return name,destination,price,type                                                      #Zwracam wszystkie wartosci
 
+def addtrip():
+    continent=input("Podaj kontynent, na którym znajduje się cel: ")                 #Pobieram kontynent
+    while continent not in destinations:                                             #Sprawdzam czy dobrze podal
+        continent = input("Podałeś zły kontynent(eu, am, az, af, au): ")               #Jak zle to powtarza
+    country=input("Podaj kraj, do którego jest ta wycieczka: ")                     #Pobieram kraj
+    city=input("Podaj miasto, do którego jest wycieczka: ")                         #Pobieram miasto
+    price=int(input("Podaj cenę wycieczki: "))                                          #Pobieram cene
+    while price<1:                                                                          #Sprawdzam czy dobrze podal
+        price=input("Nie możesz mieć ujemnego budżetu: ")                                   #Daje kolejna szanse
+    type=input("Podaj typ wycieczki: ")                                                     #Pobieram typ
+    while type not in types:  # Sprawdzam czy dobrze podal                                  #Sprawdzam typ
+        type = input("T-turystyczna I-imprezowa C-chillowa. Wybierz swój typ: ")  # Daje kolejna szanse
+    length=int(input("Ile dni trwa ta wycieczka: "))                                    #Chce dlugosc wycieczki
+    while length<1:                                                                          #Sprawdzam czy dobrze podal
+        length=input("Nie może mieć mniej niż 1 dzień: ")                                   #Jak zle to powtarza
+    sql = "INSERT INTO wycieczki (kontynent, kraj, miasto, cena, dni, typ ) VALUES (%s, %s, %s, %s, %s, %s)"    #Kwerenda insert
+    val = (continent, country, city, price, length,type)                                                    #Wartosci do wstawienia
+    mycursor.execute(sql, val)                                                                      #Wykonanie kwerendy
+    mydb.commit()                                                                       #Wyslanie do bazy
 
-
-#client_data=list(take_data())
-
-def databaseOperation():
-    mycursor.execute("SELECT * FROM produkty;")     #wykonuje kwerende
+def searchtrip(info):
+    sql="SELECT kontynent, kraj, miasto, cena, dni, typ FROM wycieczki WHERE kontynent=%s AND cena<=%s AND typ=%s" #kwerenda wybierajaca po kryteriach
+    val=(info[1],info[2],info[3])                                                                                   #wartosci do porownania
+    mycursor.execute(sql,val)     #wykonuje kwerende
     result=mycursor.fetchall()                  #zbieram wynik
-    #for x in result:                        #wypisuje
-       # print(x)
+    if len(result)==0:                          #jesli otrzymalem 0 wierszy wyniku to znaczy ze nic nie spelnia warunkow
+        print("Przykro nam, "+info[0]+"\nW tym momencie nie mamy w ofercie wycieczek spełniających Twoje kryteria")
+    else:   #w innym wypadku wypisuje wynik w tabelce
+        print(info[0]+" znalezlismy dla Ciebie "+str(len(result))+" ofert. Zapoznaj się z nimi:\n")
+        print("Kontynent\tKraj\tMiasto\tCena\tIlosc dni\tTyp")
+        for x in result:                        #wypisuje
+            print(x[0]+"\t"+x[1]+"\t"+x[2]+"\t"+str(x[3])+"\t"+str(x[4])+"\t"+x[5])
     return result
-
-#queryresult=databaseOperation()
 
 def writetofile(result):
     f=open("wynik.txt","a")
     f.write(str(result)+"\n")
 
-def addtrip():
-    continent=input("Podaj kontynent, na którym znajduje się cel: ")
-    while continent not in destinations:                                             #Sprawdzam czy dobrze podal
-        continent = input("Podałeś zły kontynent(eu, am, az, af, au): ")
-    country=input("Podaj kraj, do którego jest ta wycieczka: ")
-    city=input("Podaj miasto, do którego jest wycieczka: ")
-    price=int(input("Podaj cenę wycieczki: "))
-    while price<1:                                                                          #Sprawdzam czy dobrze podal
-        price=input("Nie możesz mieć ujemnego budżetu: ")                                   #Daje kolejna szanse
-    type=input("Podaj typ wycieczki: ")
-    while type not in types:  # Sprawdzam czy dobrze podal
-        type = input("T-turystyczna I-imprezowa C-chillowa. Wybierz swój typ: ")  # Daje kolejna szanse
-    length=int(input("Ile dni trwa ta wycieczka: "))
-    while length<1:                                                                          #Sprawdzam czy dobrze podal
-        length=input("Nie może mieć mniej niż 1 dzień: ")
-    sql = "INSERT INTO wycieczki (kontynent, kraj, miasto, cena, dni, typ ) VALUES (%s, %s, %s, %s, %s, %s)"
-    val = (continent, country, city, price, length,type)
-    mycursor.execute(sql, val)
-    mydb.commit()
-
+client_data=list(take_data())
+queryresult=searchtrip(client_data)
 #writetofile(queryresult)
-addtrip()
+#addtrip()
