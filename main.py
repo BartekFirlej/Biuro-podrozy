22bab9814b06602279a0aaf5824698949891941a
BartekFirlej <yari12@wp.pl>
2020-10-22 14:39:44 +0200

    Napisalem funkcje polaczenia z baza danych

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 def take_data():
-    destinations = ["eu", "am", "az", "af", "au"] #Kontynenty swiata
-    types = ['i', 't', 'c']                         #Typy wycieczek
+    destinations = ["eu", "am", "az", "af", "au"]                                           #Kontynenty swiata
+    types = ['i', 't', 'c']                                                                 #Typy wycieczek
     name=input("Dzień dobry. Witamy w programie biura podróży 'Bartek'. \nPodaj nam swoje imie: ") #Pobieram imie
     destination=input("Wybierz interesujący Cię kontynent podając dwie pierwsze litery: ")  #Pobieram kontynent
     while destination not in destinations:                                                  #Sprawdzam czy dobrze podal
@@ -13,4 +13,26 @@ def take_data():
         type = input("T-turystyczna I-imprezowa C-chillowa. Wybierz swój typ: ")            #Daje kolejna szanse
     return name,destination,price,type                                                      #Zwracam wszystkie wartosci
 
-client_data=list(take_data())
+
+
+#client_data=list(take_data())
+
+def databaseOperation():
+    import mysql.connector                  #incijuje pakiet bazy danych
+    mydb = mysql.connector.connect(         #lacze sie z baza danych
+        host="localhost",
+        user="root",
+        password="",
+        database="bartek"
+    )
+    mycursor = mydb.cursor()                #ustawiam kursor
+    mycursor.execute("SELECT * FROM produkty;")     #wykonuje kwerende
+    result=mycursor.fetchall()                  #zbieram wynik
+    for x in result:                        #wypisuje
+       # print(x)
+    return result
+
+databaseOperation()
+
+#def writetofile():
+
